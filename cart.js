3591f6d528ecbcc96ec2306db19e6225a067a5e4
Jerinislam05 <jerinislam05@gmail.com>
2023-12-09 14:14:38 +0000

    summer sale assignment in functional way

--- cart.js
+++ cart.js
@@ -1,134 +1,70 @@
-document.getElementById('prod1-amount').addEventListener('click', function(){
-    const prodName = document.getElementById('prod1-name')
-    const olElement = document.getElementById('selected-products');
-    const liElement = document.createElement('li');
-    liElement.textContent = prodName.innerText;
-    olElement.appendChild(liElement);
-    
-    const prodPrice = document.getElementById('price');
-    const prodString = prodPrice.innerText;
-    const prodNum = parseFloat(prodString);
-    const addPriceElement = document.getElementById('add-price');
-    const addPrice = addPriceElement.innerText;
-    const addPriceNum = parseFloat(addPrice);
-    const totalPrice = prodNum + addPriceNum;
-    addPriceElement.innerText = totalPrice;
+function addToCart(productName, selectedProd, productPrice, addedPrice) {
+  const prodName = document.getElementById(productName);
+  const olElement = document.getElementById(selectedProd);
+  const liElement = document.createElement("li");
+  liElement.textContent = prodName.innerText;
+  olElement.appendChild(liElement);
 
+  const prodPrice = document.getElementById(productPrice);
+  const prodString = prodPrice.innerText;
+  const prodNum = parseFloat(prodString);
+  const addPriceElement = document.getElementById(addedPrice);
+  const addPrice = addPriceElement.innerText;
+  const addPriceNum = parseFloat(addPrice);
+  const totalPrice = prodNum + addPriceNum;
+  addPriceElement.innerText = totalPrice;
+}
+
+document.getElementById("prod1-amount").addEventListener("click", function () {
+  addToCart("prod1-name", "selected-products", "price", "add-price");
 });
 
-document.getElementById('prod2-amount').addEventListener('click', function(){
-    const prodName = document.getElementById('prod2-name');
-    const olElement = document.getElementById('selected-products');
-    const liElement = document.createElement('li');
-    liElement.textContent = prodName.innerText;
-    olElement.appendChild(liElement);
-
-    const addPriceElement = document.getElementById('add-price');
-    const addPrice = addPriceElement.innerText;
-    const addPriceNum = parseFloat(addPrice);
-    const prodPrice2 = document.getElementById('price-second');
-    const prod2Num = prodPrice2.innerText;
-    const prod2NumString = parseFloat(prod2Num);
-    const totalPrice = addPriceNum + prod2NumString;
-    addPriceElement.innerText = totalPrice;
+document.getElementById("prod2-amount").addEventListener("click", function () {
+  addToCart("prod2-name", "selected-products", "price-second", "add-price");
 });
 
-document.getElementById('prod3-amount').addEventListener('click', function() {
-    
-    const prodName = document.getElementById('prod3-name');
-    const olElement = document.getElementById('selected-products');
-    const liElement = document.createElement('li');
-    liElement.textContent = prodName.innerText;
-    olElement.appendChild(liElement);
-
-    const addPriceElement = document.getElementById('add-price');
-    const addPrice = addPriceElement.innerText;
-    const addPriceNum = parseFloat(addPrice);
-    const prodPrice3 = document.getElementById('price-third');
-    const prod3Num = prodPrice3.innerText;
-    const prod3NumString = parseFloat(prod3Num);
-    const totalPrice = addPriceNum + prod3NumString;
-    addPriceElement.innerText = totalPrice;
+document.getElementById("prod3-amount").addEventListener("click", function () {
+  addToCart("prod3-name", "selected-products", "price-third", "add-price");
 });
 
-document.getElementById('prod4-amount').addEventListener('click',function(){
-    const prodName = document.getElementById('prod4-name');
-    const olElement = document.getElementById('selected-products');
-    const liElement = document.createElement('li');
-    liElement.textContent = prodName.innerText;
-    olElement.appendChild(liElement);
-
-    const addPriceElement = document.getElementById('add-price');
-    const addPrice = addPriceElement.innerText;
-    const addPriceNum = parseFloat(addPrice);
-    const prodPrice4 = document.getElementById('price-forth');
-    const prod4NumString = prodPrice4.innerText;
-    const prod4Num = parseFloat(prod4NumString);
-    const totalPrice = addPriceNum + prod4Num;
-    addPriceElement.innerText = totalPrice;
+document.getElementById("prod4-amount").addEventListener("click", function () {
+  addToCart("prod4-name", "selected-products", "price-forth", "add-price");
 });
 
-document.getElementById('prod5-amount').addEventListener('click',function(){
-    const prodName = document.getElementById('prod5-name');
-    const olElement = document.getElementById('selected-products');
-    const liElement = document.createElement('li');
-    liElement.textContent = prodName.innerText;
-    olElement.appendChild(liElement);
-
-    const addPriceElement = document.getElementById('add-price');
-    const addPrice = addPriceElement.innerText;
-    const addPriceNum = parseFloat(addPrice);
-    const prodPrice5 = document.getElementById('price-fifth');
-    const prod5NumString = prodPrice5.innerText;
-    const prod5Num = parseFloat(prod5NumString);
-    const totalPrice = addPriceNum + prod5Num;
-    addPriceElement.innerText = totalPrice;
+document.getElementById("prod5-amount").addEventListener("click", function () {
+  addToCart("prod5-name", "selected-products", "price-fifth", "add-price");
 });
 
-document.getElementById('prod6-amount').addEventListener('click',function(){
-    const prodName = document.getElementById('prod6-name');
-    const olElement = document.getElementById('selected-products');
-    const liElement = document.createElement('li');
-    liElement.textContent = prodName.innerText;
-    olElement.appendChild(liElement);
-
-    const addPriceElement = document.getElementById('add-price');
-    const addPrice = addPriceElement.innerText;
-    const addPriceNum = parseFloat(addPrice);
-    const prodPrice6 = document.getElementById('price-sixth');
-    const prod6NumString = prodPrice6.innerText;
-    const prod6Num = parseFloat(prod6NumString);
-    const totalPrice = addPriceNum + prod6Num;
-    addPriceElement.innerText = totalPrice;
+document.getElementById("prod6-amount").addEventListener("click", function () {
+  addToCart("prod6-name", "selected-products", "price-sixth", "add-price");
 });
 
-document.getElementById('btn-apply').addEventListener('click', function(){
-    const totalPrice = document.getElementById('add-price');
-    const totalPriceNum = parseFloat(totalPrice.innerText);
-    const couponInputField = document.getElementById('coupon-input');
-    const couponInputFieldValue = couponInputField.value;
-    
-    
-    if (totalPriceNum >= 200 && couponInputFieldValue == 'SELL200') {
-        const discount = totalPriceNum * 0.2;
-        const afterDiscount = totalPriceNum - discount;
-        console.log(afterDiscount);
-        const discountPrice = document.getElementById('add-discount');
+document.getElementById("btn-apply").addEventListener("click", function () {
+  const totalPrice = document.getElementById("add-price");
+  const totalPriceNum = parseFloat(totalPrice.innerText);
+  const couponInputField = document.getElementById("coupon-input");
+  const couponInputFieldValue = couponInputField.value;
 
-        discountPrice.innerText = discount;
-        const totalCost = document.getElementById('total-price');
-        totalCost.innerText = afterDiscount;
-        couponInputField.value = "";
-    }
-    return totalPriceNum;
+  if (totalPriceNum >= 200 && couponInputFieldValue == "SELL200") {
+    const discount = totalPriceNum * 0.2;
+    const afterDiscount = totalPriceNum - discount;
+    console.log(afterDiscount);
+    const discountPrice = document.getElementById("add-discount");
+
+    discountPrice.innerText = discount;
+    const totalCost = document.getElementById("total-price");
+    totalCost.innerText = afterDiscount;
+    couponInputField.value = "";
+  }
+  return totalPriceNum;
 });
 
-document.getElementById('btn-purchase').addEventListener('click', function(){
-    const totalPrice = document.getElementById('add-price');
-    const discountPrice = document.getElementById('add-discount');
-    const afterDiscountPrice = document.getElementById('total-price');
-    totalPrice.innerText = '00';
-    discountPrice.innerText = '00';
-    afterDiscountPrice.innerText = '00';
-    alert('purchase complete');
-})
+document.getElementById("btn-purchase").addEventListener("click", function () {
+  const totalPrice = document.getElementById("add-price");
+  const discountPrice = document.getElementById("add-discount");
+  const afterDiscountPrice = document.getElementById("total-price");
+  totalPrice.innerText = "00";
+  discountPrice.innerText = "00";
+  afterDiscountPrice.innerText = "00";
+  alert("purchase complete");
+});
